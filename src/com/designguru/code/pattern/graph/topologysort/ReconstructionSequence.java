7eb1a4d5281b9bdaab865d8e351154657c5f1eed
Fei Shan <shanfeishanfei@gmail.com>
2025-03-06 14:40:02 -0800

    add  算法模式

--- src/com/designguru/code/pattern/graph/topologysort/ReconstructionSequence.java
+++ src/com/designguru/code/pattern/graph/topologysort/ReconstructionSequence.java
@@ -1,5 +1,7 @@
 package com.designguru.code.pattern.graph.topologysort;
 
+import java.util.*;
+
 /**
  *
  * Given a sequence originalSeq and an array of sequences,
@@ -35,9 +37,118 @@ package com.designguru.code.pattern.graph.topologysort;
 public class ReconstructionSequence {
 
     public boolean canConstruct(int[] originalSeq, int[][] sequences) {
-        // TODO: Write your code here
-        return false;
+
+        if ( originalSeq.length == 1 && sequences.length == 1 ) {
+            return originalSeq.length == sequences[0].length && originalSeq[0] == sequences[0][0];
+        }
+
+        Map<Integer, Integer> counts = buildIndegreeMap(sequences);
+        Map<Integer, List<Integer>> adj = buildAdj(sequences);
+
+        return toplogicSort(originalSeq, counts, adj);
+
     }
 
+    boolean toplogicSort(int[] originalSeq, Map<Integer, Integer> counts, Map<Integer, List<Integer>> adj) {
+
+        List<Integer> sortedOrder = new ArrayList<>();
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for ( Map.Entry<Integer, Integer> e : counts.entrySet() ) {
+            if ( e.getValue() == 0 ) {
+                q.offer(e.getKey());
+            }
+        }
+
+        if ( originalSeq.length != counts.size() ) {
+            return false;
+        }
+
+
+        while ( !q.isEmpty() ) {
+
+            // more than one sources mean, there is more than one way to
+            if (q.size() > 1) {
+                return false;
+            }
+
+            int n = q.poll();
+
+
+            if ( originalSeq[sortedOrder.size()] != n ) {
+                return false;
+            }
+
+            sortedOrder.add(n);
+
+            List<Integer> nbs = adj.get(n);
+
+            for ( int nb : nbs ) {
+
+                counts.put(nb, counts.get(nb) - 1);
+
+                if ( counts.get(nb) == 0 ) {
+                    q.offer(nb);
+                }
+            }
+        }
+
+        return sortedOrder.size() == originalSeq.length;
+    }
+
+    Map<Integer, Integer> buildIndegreeMap(int[][] seq) {
+
+        Map<Integer, Integer> m = new HashMap<>();
+        for ( int[] s : seq ) {
+            for ( int i = 0; i < s.length - 1; ++i ) {
+                int a = s[i];
+                int b = s[i + 1];
+                m.put(a, m.getOrDefault(a,0));
+                m.put(b, m.getOrDefault(b,0) + 1);
+            }
+        }
+
+        return m;
+    }
+
+    Map<Integer, List<Integer>> buildAdj(int[][] seq) {
+
+        Map<Integer, List<Integer>> m = new HashMap<>();
+        for ( int[] s : seq ) {
+            for ( int i = 0; i < s.length - 1; ++i ) {
+                int a = s[i];
+                int b = s[i + 1];
+
+                m.putIfAbsent(b, new ArrayList<>());
+
+                List<Integer> l = m.getOrDefault(a, new ArrayList<>());
+                l.add(b);
+                m.put(a, l);
+            }
+        }
+
+        return m;
+    }
+
+   public static void main(String[] args) {
+       ReconstructionSequence sol = new ReconstructionSequence();
+
+       boolean result = sol.canConstruct(new int[] { 1 }, new int[][]{{1}});
+       System.out.println("Can we uniquely construct the sequence: " + result);
+
+
+       result = sol.canConstruct(new int[] { 1, 2, 3, 4 },
+               new int[][] { new int[] { 1, 2 }, new int[] { 2, 3 }, new int[] { 3, 4 } });
+       System.out.println("Can we uniquely construct the sequence: " + result);
+
+       result = sol.canConstruct(new int[] { 1, 2, 3, 4 },
+               new int[][] { new int[] { 1, 2 }, new int[] { 2, 3 }, new int[] { 2, 4 } });
+       System.out.println("Can we uniquely construct the sequence: " + result);
+
+       result = sol.canConstruct(new int[] { 3, 1, 4, 2, 5 },
+               new int[][] { new int[] { 3, 1, 5 }, new int[] { 1, 4, 2, 5 } });
+       System.out.println("Can we uniquely construct the sequence: " + result);
+   }
 
 }
